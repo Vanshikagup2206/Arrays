e5b2f5460c2df92f3c7598023a40ea5fb530a5ed
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-07-08 15:55:35 +0530

    commit

---
+++ RemoveDuplicates.java
@@ -0,0 +1,19 @@
+package Arrays;
+
+public class RemoveDuplicates {
+    public int removeDuplicates(int[] nums) {
+        int j = 0;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[j]){
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j+1;
+    }
+    public static void main(String args[]){
+        int[] nums = {-2, 2, 4, 4, 4, 4, 5, 5};
+        RemoveDuplicates sol = new RemoveDuplicates();
+        System.out.println(sol.removeDuplicates(nums));
+    }
+}
