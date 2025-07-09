828c367c7f67c037c5ba7ec84062bb96656984e1
Vanshika Gupta <vanshikagupta0009@gmail.com>
2025-07-09 14:51:40 +0530

    commit

---
+++ Union.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+import java.util.*;
+
+public class Union {
+    public int[] unionArray(int[] nums1, int[] nums2) {
+        TreeSet<Integer> union = new TreeSet<>();
+        for(int i = 0; i < nums1.length; i++){
+            union.add(nums1[i]);
+        }
+        for(int i = 0; i < nums2.length; i++){
+            union.add(nums2[i]);
+        }
+        int[] result = new int[union.size()];
+        int i = 0;
+        for (int num : union) {
+            result[i++] = num;
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        int[] nums1 = {1, 2, 3, 4, 5};
+        int[] nums2 = {1, 2, 7};
+        Union sol = new Union();
+        System.out.println(Arrays.toString(sol.unionArray(nums1, nums2)));
+    }
+}
